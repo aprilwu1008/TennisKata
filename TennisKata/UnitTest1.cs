d38cc834c9b2b4a55d1fde4f05528a3fa521e3e7
aprilwu1008 <april.wu@titansoft.com.sg>
2019-06-11 21:56:51 +0800

    Second TennisKata.finished 0611.

--- TennisKata/UnitTest1.cs
+++ TennisKata/UnitTest1.cs
@@ -5,6 +5,11 @@ namespace TennisKata
 {
     public class Tennis
     {
+        public string Adv(string a)
+        {
+            return a + " Adv";
+        }
+
         public bool CheckSameOrNot(int a, int b)
         {
             if (a == b)
@@ -14,9 +19,50 @@ namespace TennisKata
             return false;
         }
 
+        public string DiffScoreReport(int a, int b)
+        {
+            var result = "";
+            if (a <= 3 && b <= 3)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    var count = i == 0 ? a : b;
+                    switch (count)
+                    {
+                        case 0:
+                            result += count.ToString();
+                            break;
+
+                        case 1:
+                            result += count.ToString() + "5";
+                            break;
+
+                        default:
+                            result += (count + 1).ToString() + "0";
+                            break;
+                    }
+
+                    result += i == 0 ? ":" : "";
+                }
+
+                return result;
+            }
+
+            return result + "one score is over 40.";
+        }
+
+        public bool isAdv(int a, int b)
+        {
+            if (Math.Abs(a - b) == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
         public bool isDeuce(int a, int b)
         {
-            if (a > 3 && b > 3)
+            if (a > 2 && b > 2)
             {
                 if (a == b)
                 {
@@ -40,20 +86,20 @@ namespace TennisKata
             var str = "";
             switch (a)
             {
+                case 0:
+                    str = "0:0";
+                    break;
+
                 case 1:
                     str = "15:15";
                     break;
 
                 case 2:
-                    str = "20:20";
-                    break;
-
-                case 3:
                     str = "30:30";
                     break;
 
                 default:
-                    str = "The result is not all.";
+                    str = "The result is over 40.";
                     break;
             }
 
@@ -66,27 +112,57 @@ namespace TennisKata
 
             return str;
         }
+
+        public string Win(string a)
+        {
+            return a + " Wins";
+        }
     }
 
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
+        public void Adv()
+        {
+            var tennis = new Tennis();
+            var adv = tennis.Adv("April");
+            Assert.AreEqual("April Adv", adv);
+        }
+
+        [TestMethod]
         public void CheckSameOrNot()
         {
             var tennis = new Tennis();
-            var isSame = tennis.CheckSameOrNot(3, 3);
+            var isSame = tennis.CheckSameOrNot(2, 2);
             Assert.AreEqual(true, isSame);
+        }
+
+        [TestMethod]
+        public void DiffScoreReport()
+        {
+            var tennis = new Tennis();
+            var diffScoreReport = tennis.DiffScoreReport(5, 7);
+            Assert.AreEqual("one score is over 40.", diffScoreReport);
+        }
+
+        [TestMethod]
+        public void isAdv()
+        {
+            var tennis = new Tennis();
+            var isAdv = tennis.isAdv(3, 2);
+            Assert.AreEqual(true, isAdv);
         }
 
         [TestMethod]
         public void isDeuce()
         {
             var tennis = new Tennis();
-            var deuce = tennis.isDeuce(4, 4);
+            var deuce = tennis.isDeuce(3, 3);
             Assert.AreEqual(true, deuce);
         }
 
+        [TestMethod]
         public void isGamePoint()
         {
             var tennis = new Tennis();
@@ -98,7 +174,7 @@ namespace TennisKata
         public void ResultAll()
         {
             var tennis = new Tennis();
-            var resultAll = tennis.ResultAll(3, 3);
+            var resultAll = tennis.ResultAll(2, 2);
             Assert.AreEqual("30:30", resultAll);
         }
 
@@ -108,6 +184,14 @@ namespace TennisKata
             var tennis = new Tennis();
             var resultDeuce = tennis.ResultDeuce(true);
             Assert.AreEqual("Deuce", resultDeuce);
+        }
+
+        [TestMethod]
+        public void Win()
+        {
+            var tennis = new Tennis();
+            var win = tennis.Win("April");
+            Assert.AreEqual("April Wins", win);
         }
     }
 }
